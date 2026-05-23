2edbc039d846fb0c4d4423063c2bd197fec44013
Giap Minh Phu <giapminhphu1@gmail.com>
2026-05-23 23:27:21 +0700

    config mesage

--- src/main/java/com/swp5/library_management/Hello.java
+++ src/main/java/com/swp5/library_management/Hello.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class Hello {
     @GetMapping("/hello")
     public String sayHello(){
-       return "Hello World!";
+       return "Hello World hahaah!";
     }
 }
