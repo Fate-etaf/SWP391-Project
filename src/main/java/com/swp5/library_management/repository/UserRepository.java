23c28bd1134aae7b87add29696473c468bfa963e
Giap Minh Phu <giapminhphu1@gmail.com>
2026-06-01 16:09:19 +0700

    fix bug

--- src/main/java/com/swp5/library_management/repository/UserRepository.java
+++ src/main/java/com/swp5/library_management/repository/UserRepository.java
@@ -2,14 +2,11 @@ package com.swp5.library_management.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import java.util.Optional;
 
 import com.swp5.library_management.entity.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, String> {
-<<<<<<< HEAD
-
     boolean existsByUserId(String userId);
 
     boolean existsByEmail(String email);
@@ -20,9 +17,4 @@ public interface UserRepository extends JpaRepository<User, String> {
      * Spring Data JPA tự sinh: SELECT COUNT(*) FROM Users WHERE Status = ?
      */
     long countByStatus(String status);
-=======
-    
-    // Tự động sinh câu lệnh SQL: SELECT * FROM Users WHERE UserID = ? AND Email = ? AND CampusID = ?
-    Optional<User> findByUserIdAndEmailAndCampusId(String userId, String email, Integer campusId);
->>>>>>> loginbranch
 }
